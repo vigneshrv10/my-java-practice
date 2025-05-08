e915bd0aa59e83b6d6fd06631b5671af9ee08984
vigneshrv10 <vigneshravikrindi05@gmail.com>
2025-05-08 11:19:55 +0530

    update small

--- Floyd_triangle.java
+++ Floyd_triangle.java
@@ -1,12 +1,13 @@
-import java.util.*;
 public class Floyd_triangle {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int start = 0;
-        for (int i=1;i<=4;i++){
-
+        int start = 1;
+        for (int i=1;i<=5;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print(start+" ");
+                start++;
+            }
+            System.out.println();
         }
-        sc.close();
     }
     
 }
