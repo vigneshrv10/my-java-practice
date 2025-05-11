6b0466b7736e3ab29bbc4fc65b908102a42af4ec
vigneshrv10 <vigneshravikrindi05@gmail.com>
2025-05-11 09:41:16 +0530

    functions practice also completed

---
+++ Mathclass.java
@@ -0,0 +1,10 @@
+public class Mathclass {
+    public static void main(String[] args) {
+        System.out.println(Math.min(3.5,6));
+        System.out.println(Math.max(69,69.5));
+        System.out.println(Math.sqrt(2));
+        System.out.println(Math.pow(2,5));
+        System.out.println(Math.abs(5));
+    }
+    
+}
